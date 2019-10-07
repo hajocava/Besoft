755648a24f791ab0a9f3097b4d81ffa1b06fe36e
Haziel <hajocava@gmail.com>
2019-10-07 13:02:59 -0500

    Pruebas

--- src/components/Head/Nav.js
+++ src/components/Head/Nav.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 import anime from 'animejs'
+import getWindowDimensions from '../Workflow/getWindowDimensions'
 
 
 export default function Nav() {
@@ -15,11 +17,12 @@ export default function Nav() {
 
     const showMenuMovil = () => setState({ ...state, menuMovil: !menuMovil })
 
+
     useEffect(() => {
         function handleScroll() {
             const currentScrollPos = window.pageYOffset
 
-            if(currentScrollPos > 50)
+            if (currentScrollPos > 50)
                 setState(state => {
                     return {
                         ...state,
@@ -41,35 +44,35 @@ export default function Nav() {
 
         window.addEventListener('scroll', handleScroll)
         return () => window.removeEventListener('scroll', handleScroll)
-    },[])
+    }, [])
+
+    const { width } = getWindowDimensions();
 
     return (
-        <nav className={visible ? `${prevScroll < 90 ? '' : 'shadow'}` : 'hidden'}>
-            <div className="container">
-                <ul className="d-none d-md-flex justify-content-between align-items-center">
-                    <li>Services</li>
-                    <li>About</li>
-                    <li className="logo mb-2">Besoft</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </ul>
-                <div className="movil d-flex d-md-none justify-content-between align-items-center">
-                    <div className="logo d-md-none mb-2">
-                        Besoft
-                    </div>
-                    <div onClick={showMenuMovil} className={`icono-menu d-md-none d-flex flex-column ${menuMovil ? 'active' : ''}`}>
-                        <span className="d-block"></span>
-                        <span className="d-block align-self-end"></span>
-                    </div>
-                </div>
-                <div className={`slide d-none d-md-none ${menuMovil ? 'active d-flex align-items-center justify-content-end' : ''}`}>
-                    <ul className="d-flex flex-column align-items-end mr-4">
+        <nav className={visible || menuMovil ? `${prevScroll < 90 ? '' : 'shadow'}` : 'hidden'}>
+            <div className={`logo movil ${menuMovil ? 'active' : ''}`}>Besoft</div>
+            <div className={`${width > 767 ? 'container' : ''}`}>
+                <ul className={`nav-links ${menuMovil ? 'open' : ''}`}>
+                    <NavLink to={"#"}>
                         <li>Services</li>
+                    </NavLink>
+                    <NavLink to={"#"}>
                         <li>About</li>
+                    </NavLink>
+                    <NavLink className="d-none d-md-inline" to={"#"}>
+                        <li className="logo mb-2">Besoft</li>
+                    </NavLink>
+                    <NavLink to={"#"}>
                         <li>Projects</li>
+                    </NavLink>
+                    <NavLink to={"#"}>
                         <li>Contact</li>
-                    </ul>
-                </div>
+                    </NavLink>
+                </ul>
+            </div>
+            <div onClick={showMenuMovil} className={`hamburguer ${menuMovil ? 'active' : ''}`}>
+                <span className="d-block"></span>
+                <span className="d-block align-self-end"></span>
             </div>
         </nav>
     )
